3516481dc3b1a23d3b405635e1bad79b9fa0c1f1
Meltem <melltemkilic@gmail.com>
2017-04-18 17:28:52 -0400

    the code added to make the counter red when character length surpasses 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
   $('.new-tweet').on('keyup', '#compose textarea', function() {
     var charactersTyped = $(this).val().length;
     var charactersLeft = 140 - charactersTyped;
-    $(this).siblings('span').find('.counter').html(charactersLeft);
+    var updatedCounter = $(this).siblings('span').find('.counter').html(charactersLeft);
+    if (charactersLeft < 0) {
+      updatedCounter.addClass('color-red');
+    } else {
+      updatedCounter.removeClass('color-red');
+    }
   });
-})
+});
