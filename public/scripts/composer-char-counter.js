2360e921e034790a5167f0a74a38be24ac045a83
Meltem <melltemkilic@gmail.com>
2017-04-18 17:18:57 -0400

    the counter element has been targetted using jqury so that the status of the counter on the new-tweet section will be updated as the user types in

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
   $('.new-tweet').on('keyup', '#compose textarea', function() {
     var charactersTyped = $(this).val().length;
-    console.log('typed: ', charactersTyped);
     var charactersLeft = 140 - charactersTyped;
-    console.log('left: ', charactersLeft);
-  })
+    $(this).siblings('span').find('.counter').html(charactersLeft);
+  });
 })
