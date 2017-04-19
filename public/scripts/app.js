00d91ec569da98b6ca1c3e3e1418ffe6ca8af035
Meltem <melltemkilic@gmail.com>
2017-04-18 20:47:07 -0400

    rendertweets function implemented taking in an array of tweet objects and then appending each one to the #tweets-container. It leverages createTweetElement function whiand append the return value of it to the#tweets-container section.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,58 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-var tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// Fake data taken from tweets.json
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
 $(document).ready(function() {
 
   function createTweetElement (tweetObj) {
+    $tweet = $("<article>").addClass("tweet");
     let html = `
       <header>
-        <img src=${tweetObj.user.avatars.small} alt="bird" />
+        <img src=${tweetObj.user.avatars.small} alt="user-avatar" />
         <h1>${tweetObj.user.name}</h1>
         <h2>${tweetObj.user.handle}</h2>
       </header>
@@ -40,13 +72,17 @@ $(document).ready(function() {
         </span>
       </footer>
     `;
-    $tweet = $("<article>").addClass("tweet");
-    return $tweet.append(html);
+    $tweet = $tweet.append(html);
+    return $tweet;
   }
 
-  var $tweet = createTweetElement(tweetData);
+  function renderTweets(tweets) {
+    tweets.forEach((tweet)=> {
+      var a = createTweetElement(tweet);
+      $(".tweets-container").append(a);
+    });
+  }
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
+  
 });
