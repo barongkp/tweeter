bd1327a439f6fbb9f500957b8157d8ca33fd929b
Meltem <melltemkilic@gmail.com>
2017-04-21 00:58:20 +0000

    saveTweets function has been modified. It's saving tweets to the mongodb now

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,20 +6,27 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-      callback(null, db.collection("tweets").find({}).sort(sortNewestFirst));
+      console.log('I\'m inside getTweets function callback');
+      db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      console.log('callback will get fired now');
+      callback(null, tweets);
+      console.log("tweets", tweets);
+      console.log('end of the get tweets function');
+      });
+      // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
     }
-
-  };
+  }
 }
